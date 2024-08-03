957e2115e8f18c715e8865895937790f9b56ceaf
leeobby <leeobbyw@gmail.com>
2024-08-03 13:22:04 +0700

    docs: column structure

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+import data from "./data.json";
 
-document.querySelector('#app').innerHTML = `
+console.log("%cHello, World!", "color: blue; font-size: 20px;");
+console.log(
+  "%cWelcome to My Fancy Console!",
+  "color: white; background-color: #007acc; font-size: 24px; padding: 10px; border-radius: 5px;"
+);
+console.log(
+  "%c✨ JavaScript is Awesome! ✨",
+  "font-weight: bold; font-size: 40px; color: red; text-shadow: 2px 2px 0 rgba(255,0,0,0.5), 4px 4px 0 rgba(255,165,0,0.5), 6px 6px 0 rgba(255,255,0,0.5), 8px 8px 0 rgba(0,128,0,0.5), 10px 10px 0 rgba(0,0,255,0.5);"
+);
+console.log(
+  "%c🎉 Welcome to My Fancy Consolae! 🎉",
+  "font-weight:bold; font-size:30px; color:#fff; background-color:#007acc; padding:15px; border-radius:10px;"
+);
+
+console.log(data);
+
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -19,6 +36,6 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
