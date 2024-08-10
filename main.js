9cd42de84d3b5a41efd1ae4487bae729a6188916
leeobby <leeobbyw@gmail.com>
2024-08-10 13:04:38 +0700

    feat: add address book list

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
 import "./style.css";
-import { setupCounter } from "./counter.js";
 import dataContacts from "./data.json";
 
-console.log(dataContacts);
+const AddressBookService = () => {
+  const collectionOfAddresses = document.querySelector("#addresses");
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <h1>Address Book</h1>
-    
-    ${dataContacts.map((contact) => {
-      return `<div>
-        <h2>${contact.fullName}</h2>
-      </div>`;
-    })}
-  </div>
-`;
+  function resetAddressBook() {
+    collectionOfAddresses.innerHTML = "";
+  }
+
+  function getAddressBook() {
+    const addresses = document.getElementById("addresses");
+    dataContacts.forEach((contact) => {
+      const article = document.createElement("article");
+      article.className =
+        "w-full flex flex-col border border-gray-500 rounded p-4";
+
+      const title = document.createElement("h1");
+      title.className = "font-medium text-xl";
+      title.textContent = contact.fullName;
+
+      const box = document.createElement("address");
+
+      const phone = document.createElement("p");
+      phone.textContent = contact.phone;
+      const email = document.createElement("p");
+      email.textContent = contact.email;
+      const address = document.createElement("p");
+      address.textContent = contact.address;
+
+      box.append(phone, email, address);
+
+      article.append(title, box);
+
+      addresses.append(article);
+    });
+  }
+
+  return {
+    render: () => {
+      getAddressBook();
+    },
+  };
+};
+
+AddressBookService().render();
