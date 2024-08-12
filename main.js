389cc3fa923eb5a0cffeccced441546961024591
leeobby <leeobbyw@gmail.com>
2024-08-12 23:50:06 +0700

    feat: add create address book

--- main.js
+++ main.js
@@ -4,13 +4,46 @@ import dataContacts from "./data.json";
 const AddressBookService = () => {
   const collectionOfAddresses = document.querySelector("#addresses");
 
+  document.querySelector("#addContact").addEventListener("click", () => {
+    document.querySelector("#modal").classList.remove("hidden");
+  });
+
+  document.querySelector("#btnCloseModal").addEventListener("click", () => {
+    modal.classList.add("hidden");
+  });
+
+  document.querySelector("#btnSave").addEventListener("click", () => {
+    const input = document.querySelectorAll("#form input");
+
+    if (
+      !document.querySelector("input[name='fullname']").value ||
+      !document.querySelector("input[name='phone']").value ||
+      !document.querySelector("input[name='email']").value ||
+      !document.querySelector("input[name='address']").value
+    ) {
+      alert("field cannot be empty!");
+      return;
+    }
+
+    const data = {
+      id: document.querySelector("input[name='id']").value || Date.now(),
+      fullName: document.querySelector("input[name='fullname']").value,
+      phone: document.querySelector("input[name='phone']").value,
+      email: document.querySelector("input[name='email']").value,
+      address: document.querySelector("input[name='address']").value,
+    };
+
+    saveContacts(data);
+  });
+
   function resetAddressBook() {
     collectionOfAddresses.innerHTML = "";
   }
 
-  function getAddressBook() {
+  function getAddressBook(data) {
+    resetAddressBook();
     const addresses = document.getElementById("addresses");
-    dataContacts.forEach((contact) => {
+    data.forEach((contact) => {
       const article = document.createElement("article");
       article.className =
         "w-full flex flex-col border border-gray-500 rounded p-4";
@@ -36,11 +69,75 @@ const AddressBookService = () => {
     });
   }
 
+  function saveContacts(data) {
+    const currentContacts = JSON.parse(localStorage.getItem("contacts")) || [];
+
+    const contactIsExist = currentContacts.find((val) => val.id === data.id);
+
+    if (contactIsExist) {
+      alert("contact is already exist!");
+      return;
+    }
+
+    currentContacts.push(data);
+
+    localStorage.setItem("contacts", JSON.stringify(currentContacts));
+
+    getAddressBook(currentContacts);
+
+    document.querySelector("#modal").classList.add("hidden");
+  }
+
+  function editContact(data) {
+    const currentContacts = localStorage.getItem("contacts");
+
+    const contactIsExist = JSON.parse(currentContacts).find(
+      (val) => val.id === data.id
+    );
+
+    if (!contactIsExist) {
+      alert("contact is not found");
+      return;
+    }
+
+    contactIsExist = data;
+
+    localStorage.setItem("contacts", JSON.stringify(currentContacts));
+
+    return;
+  }
+
+  function deleteContact(id) {
+    const currentContacts = localStorage.getItem("contacts");
+
+    const contactIsExist = JSON.parse(currentContacts).find(
+      (val) => val.id === data.id
+    );
+
+    if (!contactIsExist) {
+      alert("contact is not found");
+      return;
+    }
+
+    const index = currentContacts.indexOf(contactIsExist);
+
+    JSON.parse(currentContacts).splice(index, 1);
+
+    localStorage.setItem("contacts", JSON.stringify(currentContacts));
+    return;
+  }
+
   return {
-    render: () => {
-      getAddressBook();
+    render: (data) => {
+      getAddressBook(data);
     },
   };
 };
 
-AddressBookService().render();
+const currentContacts = localStorage.getItem("contacts");
+
+const dataContactStorage = currentContacts
+  ? JSON.parse(currentContacts)
+  : dataContacts;
+
+AddressBookService().render(dataContactStorage);
