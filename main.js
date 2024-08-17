49de1187b35ef3c04bfa91351ff4490500b24e4b
leeobby <leeobbyw@gmail.com>
2024-08-17 12:06:59 +0700

    feat: add edit and search

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 import "./style.css";
-import dataContacts from "./data.json";
 
 const AddressBookService = () => {
   const collectionOfAddresses = document.querySelector("#addresses");
 
   document.querySelector("#addContact").addEventListener("click", () => {
+    resetForm();
     document.querySelector("#modal").classList.remove("hidden");
   });
 
   document.querySelector("#btnCloseModal").addEventListener("click", () => {
-    modal.classList.add("hidden");
+    document.querySelector("#modal").classList.add("hidden");
+  });
+
+  document.querySelector("#btnReset").addEventListener("click", () => {
+    resetAddressBook();
+    const currentContacts = JSON.parse(localStorage.getItem("contacts")) || [];
+
+    getAddressBook(currentContacts);
+  });
+
+  document.querySelector("#btnSearch").addEventListener("click", () => {
+    const searchValue = document.querySelector(
+      "input[name='searchInput']"
+    ).value;
+
+    const currentContacts = JSON.parse(localStorage.getItem("contacts")) || [];
+
+    const filtered = currentContacts.filter((val) => {
+      return (
+        val.fullName.toLowerCase() == searchValue.toLowerCase() ||
+        val.phone.toLowerCase() == searchValue.toLowerCase() ||
+        val.email.toLowerCase() == searchValue.toLowerCase() ||
+        val.address.toLowerCase() == searchValue.toLowerCase()
+      );
+    });
+
+    getAddressBook(filtered);
   });
 
   document.querySelector("#btnSave").addEventListener("click", () => {
-    const input = document.querySelectorAll("#form input");
-
     if (
       !document.querySelector("input[name='fullname']").value ||
       !document.querySelector("input[name='phone']").value ||
@@ -25,19 +49,23 @@ const AddressBookService = () => {
       return;
     }
 
-    const data = {
-      id: document.querySelector("input[name='id']").value || Date.now(),
-      fullName: document.querySelector("input[name='fullname']").value,
-      phone: document.querySelector("input[name='phone']").value,
-      email: document.querySelector("input[name='email']").value,
-      address: document.querySelector("input[name='address']").value,
-    };
-
-    saveContacts(data);
+    saveContacts();
   });
+
+  function resetForm() {
+    document.querySelector("input[name='id']").value = "";
+    document.querySelector("input[name='fullname']").value = "";
+    document.querySelector("input[name='phone']").value = "";
+    document.querySelector("input[name='email']").value = "";
+    document.querySelector("input[name='address']").value = "";
+  }
 
   function resetAddressBook() {
     collectionOfAddresses.innerHTML = "";
+  }
+
+  function saveToLs(key, values) {
+    localStorage.setItem(key, JSON.stringify(values));
   }
 
   function getAddressBook(data) {
@@ -45,12 +73,37 @@ const AddressBookService = () => {
     const addresses = document.getElementById("addresses");
     data.forEach((contact) => {
       const article = document.createElement("article");
-      article.className =
-        "w-full flex flex-col border border-gray-500 rounded p-4";
+      article.className = "p-2 border border-gray-500 rounded";
+
+      const header = document.createElement("div");
+      header.className = "flex items-center";
 
       const title = document.createElement("h1");
-      title.className = "font-medium text-xl";
       title.textContent = contact.fullName;
+      title.className = "flex-1 font-medium text-xl";
+
+      const btnEdit = document.createElement("span");
+      btnEdit.textContent = "Edit";
+      btnEdit.className = "p-2 text-yellow-500 cursor-pointer";
+      btnEdit.addEventListener("click", () => {
+        document.querySelector("input[name='id']").value = contact.id;
+        document.querySelector("input[name='fullname']").value =
+          contact.fullName;
+        document.querySelector("input[name='phone']").value = contact.phone;
+        document.querySelector("input[name='email']").value = contact.email;
+        document.querySelector("input[name='address']").value = contact.address;
+        document.querySelector("#modal").classList.remove("hidden");
+      });
+
+      const btnDelete = document.createElement("span");
+      btnDelete.textContent = "Delete";
+      btnDelete.className = "p-2 text-rose-500 cursor-pointer";
+      btnDelete.addEventListener("click", () => {
+        if (confirm("Are you sure ?")) {
+          deleteContact(contact.id);
+          return;
+        }
+      });
 
       const box = document.createElement("address");
 
@@ -63,55 +116,58 @@ const AddressBookService = () => {
 
       box.append(phone, email, address);
 
-      article.append(title, box);
+      header.append(title, btnEdit, btnDelete);
+
+      article.append(header, box);
 
       addresses.append(article);
     });
   }
 
-  function saveContacts(data) {
+  function saveContacts() {
+    const currentValues = {
+      id: document.querySelector("input[name='id']").value,
+      fullName: document.querySelector("input[name='fullname']").value,
+      phone: document.querySelector("input[name='phone']").value,
+      email: document.querySelector("input[name='email']").value,
+      address: document.querySelector("input[name='address']").value,
+    };
+
     const currentContacts = JSON.parse(localStorage.getItem("contacts")) || [];
 
-    const contactIsExist = currentContacts.find((val) => val.id === data.id);
+    const isExist = currentContacts.find((val) => {
+      return parseInt(val.id) === parseInt(currentValues.id);
+    });
 
-    if (contactIsExist) {
-      alert("contact is already exist!");
+    if (isExist) {
+      isExist.fullName = currentValues.fullName;
+      isExist.phone = currentValues.phone;
+      isExist.email = currentValues.email;
+      isExist.address = currentValues.address;
+
+      saveToLs("contacts", currentContacts);
+      getAddressBook(currentContacts);
+      document.querySelector("#modal").classList.add("hidden");
+      return;
+    } else {
+      currentValues.id = Date.now();
+
+      currentContacts.push(currentValues);
+
+      saveToLs("contacts", currentContacts);
+
+      getAddressBook(currentContacts);
+
+      document.querySelector("#modal").classList.add("hidden");
       return;
     }
-
-    currentContacts.push(data);
-
-    localStorage.setItem("contacts", JSON.stringify(currentContacts));
-
-    getAddressBook(currentContacts);
-
-    document.querySelector("#modal").classList.add("hidden");
-  }
-
-  function editContact(data) {
-    const currentContacts = localStorage.getItem("contacts");
-
-    const contactIsExist = JSON.parse(currentContacts).find(
-      (val) => val.id === data.id
-    );
-
-    if (!contactIsExist) {
-      alert("contact is not found");
-      return;
-    }
-
-    contactIsExist = data;
-
-    localStorage.setItem("contacts", JSON.stringify(currentContacts));
-
-    return;
   }
 
   function deleteContact(id) {
-    const currentContacts = localStorage.getItem("contacts");
+    const currentContacts = JSON.parse(localStorage.getItem("contacts"));
 
-    const contactIsExist = JSON.parse(currentContacts).find(
-      (val) => val.id === data.id
+    const contactIsExist = currentContacts.find(
+      (val) => parseInt(val.id) === parseInt(id)
     );
 
     if (!contactIsExist) {
@@ -121,9 +177,11 @@ const AddressBookService = () => {
 
     const index = currentContacts.indexOf(contactIsExist);
 
-    JSON.parse(currentContacts).splice(index, 1);
+    currentContacts.splice(index, 1);
 
-    localStorage.setItem("contacts", JSON.stringify(currentContacts));
+    saveToLs("contacts", currentContacts);
+
+    getAddressBook(currentContacts);
     return;
   }
 
@@ -136,8 +194,6 @@ const AddressBookService = () => {
 
 const currentContacts = localStorage.getItem("contacts");
 
-const dataContactStorage = currentContacts
-  ? JSON.parse(currentContacts)
-  : dataContacts;
+const dataContactStorage = currentContacts ? JSON.parse(currentContacts) : [];
 
 AddressBookService().render(dataContactStorage);
